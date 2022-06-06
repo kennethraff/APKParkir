a6ef34ca99aa89d035be1c2ae4ee7d666293a1ef
kennethraff <kraffelino@student.ciputra.ac.id>
2022-06-06 13:34:54 +0700

    APK Parkir

--- app/src/main/java/com/example/aplikasiparkir/MainActivity.kt
+++ app/src/main/java/com/example/aplikasiparkir/MainActivity.kt
@@ -19,6 +19,6 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this@MainActivity, HomeActivity::class.java)
             startActivity(intent)
             finish()
-        },10000)
+        },20000)
     }
 }
