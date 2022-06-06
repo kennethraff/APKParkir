18476366de62584275e242cb5646e547820a2c63
kennethraff <kraffelino@student.ciputra.ac.id>
2022-06-06 14:13:14 +0700

    APK Parkir

--- app/src/main/java/com/example/aplikasiparkir/HomeActivity.kt
+++ app/src/main/java/com/example/aplikasiparkir/HomeActivity.kt
@@ -10,5 +10,7 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding= ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+                supportActionBar?.hide()
     }
 }
